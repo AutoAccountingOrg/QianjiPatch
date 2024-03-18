4930b49d496d02e7534655888bafab7d15f1ba49
Ankio <ankio@ankio.net>
2024-03-18 13:51:14 +0800

    :sparkles: Dex工具类

---
+++ dex/src/main/java/net/ankio/dex/Dex.kt
@@ -0,0 +1,189 @@
+package net.ankio.dex
+
+import net.ankio.dex.model.Clazz
+import net.ankio.dex.model.ClazzField
+import net.ankio.dex.model.ClazzMethod
+import org.jf.dexlib2.DexFileFactory
+import org.jf.dexlib2.iface.DexFile
+import java.lang.reflect.Field
+
+object Dex {
+
+    fun findClazz( path: String,  classLoader: ClassLoader,  rules: List<Clazz>): HashMap<String, String> {
+        val  dexFile: DexFile = DexFileFactory.loadDexFile(path, null)
+        val results = HashMap<String, String>()
+        for (classDef in dexFile.classes) {
+            var className = classDef.type
+            className = className.substring(1, className.length - 1).replace('/', '.')
+
+            rules.forEach { itClazz ->
+                //判断包名规则有要求吗？有要求就使用正则匹配
+                val condition1 = if (itClazz.nameRule.isNotEmpty()) {
+                    Regex(itClazz.nameRule).matches(className)
+                } else {
+                    true
+                }
+                if(condition1){
+                    val clazz = classLoader.loadClass(className)
+                    val condition2 = if (itClazz.fields.isNotEmpty()) {
+                        //这里应该是判断字段是否存在吧？
+                        itClazz.fields.map { field ->
+                            findFieldIsExist(clazz, field)
+                        }.all { it }
+                    } else {
+                        true
+                    }
+                    val condition3 = if (itClazz.methods.isNotEmpty()) {
+                        itClazz.methods.map { method ->
+                            findMethodIsExist(clazz, method)
+                        }.all { it }
+                    } else {
+                        true
+                    }
+                    if (condition2 && condition3) {
+                        results[itClazz.name] = className
+                    }
+                }
+            }
+        }
+        return results
+    }
+
+    /**
+     * 查找某个字段，只能根据类型查找咯
+     */
+   fun findField(clazz: Class<*>, field: ClazzField): String {
+        //判断指定要求的字段是否在class里面
+        val fields: Array<Field> = clazz.declaredFields
+        for (f in fields) {
+            f.isAccessible = true
+            println(f.type.name)
+            if (f.type.name == field.type) {
+                return field.name
+            }
+        }
+        return ""
+    }
+
+    /**
+     * 判断某个字段是否存在
+     */
+    fun findFieldIsExist(clazz: Class<*>, field: ClazzField): Boolean {
+        if (field.name.isEmpty() && field.type.isEmpty()) {
+            return false
+        }
+        //判断指定要求的字段是否在class里面
+        val fields: Array<Field> = clazz.declaredFields
+        for (f in fields) {
+            f.isAccessible = true
+
+            val condition1 = if (field.name.isNotEmpty()) {
+                f.name == field.name
+            } else {
+                true
+            }
+
+            val condition2 = if (field.type.isNotEmpty()) {
+                f.type.name == field.type
+            } else {
+                true
+            }
+
+            return condition1 && condition2
+
+        }
+        return false
+    }
+
+    /**
+     * 查找某个方法
+     */
+    fun findMethod(clazz: Class<*>, clazzMethod: ClazzMethod): String {
+        //判断指定要求的字段是否在class里面
+        val methods = clazz.declaredMethods
+        for (method in methods) {
+
+
+            val condition2 = if (clazzMethod.returnType.isNotEmpty()) {
+                method.returnType.name == clazzMethod.returnType
+            } else {
+                true
+            }
+
+            val condition3 = if (clazzMethod.modifiers.isNotEmpty()) {
+                method.modifiers.toString() == clazzMethod.modifiers
+            } else {
+                true
+            }
+
+            val condition4 = if (clazzMethod.parameters.isNotEmpty()) {
+                val parameters = method.parameters
+                if (parameters.size == clazzMethod.parameters.size) {
+                    parameters.mapIndexed { index, parameter ->
+                        parameter.type.name == clazzMethod.parameters[index].type
+                    }.all { it }
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+
+
+            if (condition2 && condition3 && condition4) {
+                return method.name
+            }
+        }
+        return ""
+    }
+
+    /**
+     * 判断某个方法是否在给定的clazz里面
+     */
+    fun findMethodIsExist(clazz: Class<*>, clazzMethod: ClazzMethod): Boolean {
+
+        if (clazzMethod.name.isEmpty() && clazzMethod.returnType.isEmpty() && clazzMethod.modifiers.isEmpty() && clazzMethod.parameters.isEmpty()) {
+            return false
+        }
+
+        //判断指定要求的字段是否在class里面
+        val methods = clazz.declaredMethods
+        for (method in methods) {
+            val condition1 = if (clazzMethod.name.isNotEmpty()) {
+                method.name == clazzMethod.name
+            } else {
+                true
+            }
+
+            val condition2 = if (clazzMethod.returnType.isNotEmpty()) {
+                method.returnType.name == clazzMethod.returnType
+            } else {
+                true
+            }
+
+            val condition3 = if (clazzMethod.modifiers.isNotEmpty()) {
+                method.modifiers.toString() == clazzMethod.modifiers
+            } else {
+                true
+            }
+
+            val condition4 = if (clazzMethod.parameters.isNotEmpty()) {
+                val parameters = method.parameters
+                if (parameters.size == clazzMethod.parameters.size) {
+                    parameters.mapIndexed { index, parameter ->
+                        parameter.type.name == clazzMethod.parameters[index].type
+                    }.all { it }
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+
+            if (condition1 && condition2 && condition3 && condition4) {
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ dex/src/main/java/net/ankio/dex/model/Clazz.kt
@@ -0,0 +1,8 @@
+package net.ankio.dex.model
+
+data class Clazz(
+    val name:String = "",
+    val fields:List<ClazzField> = listOf(),
+    val methods:List<ClazzMethod> = listOf(),
+    val nameRule:String = "",
+)
